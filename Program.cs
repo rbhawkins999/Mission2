c2b248d5a75d43e6e713c41e72de2874f09a9bd0
ryanhawkins <ryanhawkins@RYANHAWKINS5C1C>
2024-01-17 15:33:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿//Ryan Hawkins, section 2
+//This program simulates rolling two dice and keeps track of how many time each number is rolled with both dice. The user is able to input how many times they want to roll the dice.
+
+using Mission2;
+
+//first class as mentioned in the instructions
+internal class Program
+{
+    //main method
+    private static void Main(string[] args)
+    {
+        int numberRolls = 0;
+
+        //starting the simulator and asking the user to input how many rolls they want
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine(" ");
+        Console.WriteLine("How many dice rolls would you like to simulate? ");
+
+        //reading the user input as a string for now but will later turn to integer and make sure it is an integer
+        string input = Console.ReadLine();
+
+        //checking to see if the user input was actually a valid integer. if it is, the simulator contines. if it is not, the simulator ends and tells the user there was an error
+        if (int.TryParse(input, out numberRolls))
+        {
+            // If the user inputs a valid integer, numberRolls now contains the parsed integer value and the directions continue.
+            Console.WriteLine(" ");
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + numberRolls);
+
+            //array with the reults. this is where the other class and method are called with passing the variable that contains the number of rolls
+            int[] results = rollingDie.RollDie(numberRolls);
+
+            //this displays the histogram that is made in a method below
+            DisplayHistogram(results);
+
+            //last line to conclude the game after the results are shown
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+        else
+        {
+            // If the user inputs an invalid integer, the simulation is not run and they are given an error message
+            Console.WriteLine("Invalid input. That was not a valid number");
+        }
+    }
+
+    //method to display the histogram with the results
+    private static void DisplayHistogram(int[] results)
+    {
+        //array to store the counts for totals 2 to 12
+        int[] counts = new int[13]; 
+
+        //the count occurrences of each total
+        foreach (int total in results)
+        {
+            counts[total]++;
+        }
+
+        //displays the actual histogram
+        for (int iCount = 2; iCount <= 12; iCount++)
+        {
+            //calculating the percentage, not just the number of rolls
+            int percentage = counts[iCount] * 100 / results.Length;
+            //determines how many asterisks to put based on the percentage
+            Console.WriteLine($"{iCount}: {new string('*', percentage)}");
+        }
+    }
+}
---
+++ rollingDie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//created second class according to the instructions that take the input from the user of how many times the dice will be rolled and then this class simulates actually rolling the dice and returns an array of the results of the dice rolling
+namespace Mission2
+{
+    internal class rollingDie
+    {
+        //creating the method that takes the number of rolls passed from the other class
+        public static int[] RollDie(int numberOfRolls)
+        {
+            //brings in random to be used for the dice
+            Random random = new Random();
+            int[] results = new int[numberOfRolls];
+
+            //a loop that takes the number of rolls the person is doing and rolls them for that amount of times
+            for (int iCount = 0; iCount < numberOfRolls; iCount++)
+            {
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+
+                //array of the resuts of each dice combined to total the total roll number for each row
+                results[iCount] = dice1 + dice2;
+            }
+
+            //returns the results to be given back to the first class to be able to create the hitogram later
+            return results;
+        }
+    }
+}
